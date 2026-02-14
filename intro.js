e3861097ee7aad386064bdae6b2dbd6488d5e2a1
ImAnEnvoy <sureteam460@gmail.com>
2026-02-14 17:33:11 +0100

    use variables

--- intro.js
+++ intro.js
@@ -1,3 +1,7 @@
-console.log("My name  is Ayoola Ezekiel");
-console.log("I am 30 years old");
-console.log("My favourite programming language is JavaScript");
+const full_name = "Ayoola Ezekiel";
+const age = 30;
+const fav_programing_lang = Javascript;
+
+console.log("My name  is", full_name);
+console.log("I am 30 years old", age);
+console.log("My favourite programming language is", fav_programing_lang);
